f26598fed59a2711e31163b1634a7a8b8e40af2b
Devin Lin <Dlin01@stuy.edu>
2018-11-09 06:39:42 -0500

    added add diagonal

--- WordSearch.java
+++ WordSearch.java
@@ -95,4 +95,34 @@ public class WordSearch{
     data=newBoard;
     return false;
   }
+  /**Attempts to add a given word to the specified position of the WordGrid.
+  *The word is added from top left to bottom right, must fit on the WordGrid,
+  *and must have a corresponding letter to match any letters that it overlaps.
+  *
+  *@param word is any text to be added to the word grid.
+  *@param row is the vertical locaiton of where you want the word to start.
+  *@param col is the horizontal location of where you want the word to start.
+  *@return true when the word is added successfully. When the word doesn't fit,
+  *or there are overlapping letters that do not match, then false is returned.
+  */
+  public boolean addWordDiagonal(String word,int row, int col){
+    char[][] newBoard = data;
+    int index = 0;
+    int columnIndex = col;
+    if (col - 1 + word.length() <= data[row].length && row - 1 + word.length() <= data.length){
+      for (int i = row; i < word.length(); i++){
+        if (data[i][columnIndex] == '_' || Character.toString(data[i][columnIndex]).equals(word.substring (index, index + 1))){
+          data[i][columnIndex] = word.charAt(index);
+          index ++;
+          columnIndex ++;
+        } else {
+          data = newBoard;
+          return false;
+        }
+      }
+      return true;
+    }
+    data = newBoard;
+    return false;
+  }
 }
